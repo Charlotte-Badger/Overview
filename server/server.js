8c2890e365455e2de4579eddc203d28478f5e911
Eric Knechtges <eric.t.knechtges@gmail.com>
2021-05-15 11:35:27 -0400

    Revised test files -- refactored server to help with this. App testing is broken.

--- server/server.js
+++ server/server.js
@@ -11,24 +11,23 @@ const port = 3000;
 
 app.get('/overview?:courseId', function(req, res) {
   let courseId = req.query.courseId;
-  console.log('parameters', req.query);
   db.get(courseId, function(rec) {
     console.log(rec);
-    res.send({
-      "courseId": rec[0].courseId,
-      "title": rec[0].title,
-      "tagline": rec[0].tagline,
-      "students": rec[0].students,
-      "subjects": rec[0].subjects,
-      "author": rec[0].author,
-      "language": rec[0].language,
-      "captions": rec[0].captions
-      });
+    if (rec[0] === undefined) {
+      res.status(404).send([]);
+    } else {
+      res.send({
+        "courseId": rec[0].courseId,
+        "title": rec[0].title,
+        "tagline": rec[0].tagline,
+        "students": rec[0].students,
+        "subjects": rec[0].subjects,
+        "author": rec[0].author,
+        "language": rec[0].language,
+        "captions": rec[0].captions
+        });
+    }
   });
-});
-
-app.listen(port, () => {
-  console.log(`Server is listening at port ${port}`);
 });
 
 module.exports = app;
---
+++ server/start.js
@@ -0,0 +1,12 @@
+const app = require('./server.js');
+const port = 3000;
+
+const serverInstance = app.listen(port, () => {
+  console.log(`Overview is listening at http://localhost:${port}`)
+});
+
+const closeServer = () => {
+  serverInstance.close();
+}
+
+exports.closeServer = closeServer;
