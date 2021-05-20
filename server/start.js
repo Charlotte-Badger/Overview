2e68f73a6996e2256bff586490e0a66b940dd5d8
Eric Knechtges <eric.t.knechtges@gmail.com>
2021-05-20 02:02:46 -0400

    Fixed capitalization error

--- server/start.js
+++ server/start.js
@@ -1,7 +1,7 @@
 const app = require('./server.js');
 const PORT = process.env.PORT || 3000;
 
-const serverInstance = app.listen(port, () => {
+const serverInstance = app.listen(PORT, () => {
   console.log(`Overview is listening at port ${PORT}`)
 });
 
