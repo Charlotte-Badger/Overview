deece0200a3eb7e0f1b5bf1b97a010ca921cc0c8
Eric Knechtges <eric.t.knechtges@gmail.com>
2021-06-10 23:37:22 -0400

    Infobar on separate div, CSS to change position

--- database/overview100.js
+++ database/overview100.js
@@ -7,7 +7,7 @@ const capitalize = function (string) {
 
 
 const lang = [
-  "Mandarin", "Spanish", "Hindustani", "Bengali", "Portuguese", "Russian", "Japanese", "French", "German", "Tamil", "Javanese", "Italian", "Romanian", "Greek", "Hipster"
+  'Mandarin', 'Spanish', 'Hindustani', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'French', 'German', 'Tamil', 'Javanese', 'Italian', 'Romanian', 'Greek', 'Hipster'
 ];
 
 const create = function() {
@@ -16,7 +16,7 @@ const create = function() {
   let subjects = [];
   for (let i = 1; i <= 100; i++) {
     console.log('index', i);
-    captions = ["English"];
+    captions = ['English'];
     subjects = [];
     for (let j = 1; j <= 5; j++) {
       let rand = Math.floor(Math.random() * lang.length);
@@ -26,19 +26,19 @@ const create = function() {
       subjects.push(capitalize(faker.random.word()));
     }
     data.push({
-      "courseId": i,
-      "title": faker.company.catchPhrase(), // this may actually be fetched from course content API instead -- TBD
-      "tagline": capitalize(faker.hacker.ingverb()) + ' the ' + faker.hacker.adjective() + ' ' + faker.hacker.noun(),
-      "students": Math.floor((Math.random() * 90000)) + 10000,
-      "subjects": subjects,
-      "author": Math.floor((Math.random() * 99)) + 1, // external ID
-      "thumbnail": faker.image.abstract(),
-      "language": "English",
-      "captions": captions
+      'courseId': i,
+      'title': faker.company.catchPhrase(), // this may actually be fetched from course content API instead -- TBD
+      'tagline': capitalize(faker.hacker.ingverb()) + ' the ' + faker.hacker.adjective() + ' ' + faker.hacker.noun(),
+      'students': Math.floor((Math.random() * 90000)) + 10000,
+      'subjects': subjects,
+      'author': Math.floor((Math.random() * 99)) + 1, // external ID
+      'thumbnail': faker.image.abstract(),
+      'language': 'English',
+      'captions': captions
     });
   }
   db.save(data);
-}
+};
 
 create();
 
